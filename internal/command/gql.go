8485e4d62b3e437941de5e8d4a8ae7a74ac4febd
karupanerura <karupanerura@gmail.com>
2024-04-17 12:52:52 +0900

    feat: add exprerimental GQL REPL support

--- internal/command/gql.go
+++ internal/command/gql.go
@@ -5,16 +5,18 @@ import (
 	"context"
 	"encoding/json"
 	"io"
+	"log"
 	"os"
 	"strings"
 
+	"github.com/c-bata/go-prompt"
 	"github.com/karupanerura/datastore-cli/internal/datastore"
 	"github.com/karupanerura/datastore-cli/internal/parser"
 	"google.golang.org/api/iterator"
 )
 
 type GQLCommand struct {
-	Query string `arg:"" name:"query" help:"GQL Query"`
+	Query string `arg:"" name:"query" optional:"" help:"GQL Query"`
 }
 
 func (r *GQLCommand) Run(ctx context.Context, opts Options) error {
@@ -24,7 +26,73 @@ func (r *GQLCommand) Run(ctx context.Context, opts Options) error {
 	}
 
 	qp := &parser.QueryParser{Namespace: opts.Namespace}
-	q, aq, err := qp.ParseGQL(r.Query)
+	if r.Query != "" {
+		return r.execute(ctx, client, qp, r.Query)
+	}
+
+	properties := map[string][]string{}
+	{
+		kinds, err := client.GetAll(ctx, datastore.NewQuery("__kind__").KeysOnly(), nil)
+		if err != nil {
+			return err
+		}
+		for _, kind := range kinds {
+			props, err := client.GetAll(ctx, datastore.NewQuery("__property__").Ancestor(kind).KeysOnly(), nil)
+			if err != nil {
+				return err
+			}
+			properties[kind.Name] = make([]string, len(props))
+			for _, prop := range props {
+				properties[kind.Name] = append(properties[kind.Name], prop.Name)
+			}
+		}
+	}
+
+	history := []string{}
+	exiting := false
+	for {
+		in := prompt.Input(">>> ", func(d prompt.Document) []prompt.Suggest {
+			current := strings.ToUpper(strings.TrimSpace(d.CurrentLineBeforeCursor()))
+			if i := strings.LastIndexByte(current, ';'); i != -1 {
+				current = current[i+1:]
+			}
+
+			if _, ss, isFrom := strings.Cut(current, "FROM"); isFrom {
+				ss = strings.TrimSpace(ss)
+				suggests := make([]prompt.Suggest, 0, len(properties))
+				for kind := range properties {
+					suggests = append(suggests, prompt.Suggest{Text: kind})
+				}
+				return prompt.FilterHasPrefix(suggests, ss, true)
+			}
+			return nil
+		}, prompt.OptionHistory(history))
+		if in == "" {
+			if exiting {
+				return nil
+			}
+			exiting = true
+			continue
+		} else if in == "quit" || in == "exit" {
+			return nil
+		}
+
+		history = append(history, in)
+		queries := strings.Split(in, ";")
+		for _, query := range queries {
+			if strings.TrimSpace(query) == "" {
+				continue
+			}
+
+			if err := r.execute(ctx, client, qp, query); err != nil {
+				log.Println(err)
+			}
+		}
+	}
+}
+
+func (r *GQLCommand) execute(ctx context.Context, client *datastore.Client, qp *parser.QueryParser, query string) error {
+	q, aq, err := qp.ParseGQL(query)
 	if err != nil {
 		return err
 	}
